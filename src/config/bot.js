b08dc1caa3505f8cf5622af2dd74ac37c2186ead
reaper1k92-code <reaper1k92@gmail.com>
2026-06-01 09:21:54 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "⚕️Moderating and Protecting Arkansas State Roleplay by - Empty",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 2, 
       },
     ],
   },
